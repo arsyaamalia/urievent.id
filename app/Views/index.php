36ff66e8b4a807ff2a9657fbe33c6d4ca928dd0d
Alif Hakim <nurrahman1505@gmail.com>
2022-06-13 16:06:33 +0700

    menambah utility bootsrap di index.php

--- app/Views/index.php
+++ app/Views/index.php
@@ -6,13 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Urievent</title>
     <link rel="shortcut icon" type="image/png" href="/favicon.png" />
+    <link rel="stylesheet" href="/public/css/index.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 </head>
 
 <body>
     <nav class="navbar navbar-expand-md">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Urievent</a>
+            <a class="navbar-brand text-primary" href="">Urievent</a>
 
             <!-- buat togle menu bar -->
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -48,7 +49,7 @@
                         <input class="form-control me-2" input type="hidden" placeholder="Search" aria-label="Search">
                         <!-- logic : if 'button = submit' set input type="search" bisa pake js-->
                         <!-- bahan belajar : https://mdbootstrap.com/docs/standard/forms/search/ -->
-                        <button class="btn btn-outline" type="submit">Search <img src="\searchIcon.svg" alt="searchIcon"></button>
+                        <button class="btn btn-outline" type="submit">Search <img src="public\searchIcon.svg" alt="searchIcon"></button>
                     </form>
                 </ul>
 
@@ -66,23 +67,58 @@
 
     <!-- INTRO BOX -->
     <div class="bg-light">
-        <h2>Website No.1<br>
-            Organisasi & Komunitas Indonesia</h2>
-        <p>
-            Cari Media Partner, Sponsorship, Vendor, Venue <br>
-            UriEvent - Your Event’s Needs Here
-        </p>
+        <div class="text-light">
+            <p> dad</p>
+        </div>
+        <div class="mx-5 my-5 bg-light">
+            <h2>Website No.1<br>
+                Organisasi & Komunitas Indonesia</h2>
+            <p>
+                Cari Media Partner, Sponsorship, Vendor, Venue <br>
+                UriEvent - Your Event’s Needs Here
+            </p>
+        </div>
         <img src="" alt="">
     </div>
     <!-- END OF INTRO BOX -->
 
     <!-- WEBSITE INFO -->
     <div>
-        <div class="text-center">
-            Website No.1 Organisasi & Komunitas Indonesia
+        <div class="text-center fs-2 mt-4 fw-bolder">
+            <p>
+                Website No.1 Organisasi & Komunitas Indonesia
+            </p>
+
         </div>
         <div class="p-3 m-2 d-flex justify-content-center">
-            <div class=" bg-light m-2 ">
+            <div class="row mx-2">
+                <div class="card text-dark bg-light mb-3 mx-3" style="max-width: 18rem;">
+                    <div class="card-body">
+                        <h4>Medpart</h4>
+                        <p class="card-text">Temukan 2000+ Media Partner untuk dukung eventmu. Kamu bisa upload dan promosi organisasimu juga, lho!</p>
+                    </div>
+                </div>
+                <div class="card text-dark bg-light mb-3 mx-3" style="max-width: 18rem;">
+                    <div class="card-body">
+                        <h4>Sponsorship</h4>
+                        <p class="card-text">Temukan 2000+ Media Partner untuk dukung eventmu. Kamu bisa upload dan promosi organisasimu juga, lho!</p>
+                    </div>
+                </div>
+                <div class="card text-dark bg-light mb-3 mx-3" style="max-width: 18rem;">
+                    <div class="card-body">
+                        <h4>Vendor</h4>
+                        <p class="card-text">Temukan 2000+ Media Partner untuk dukung eventmu. Kamu bisa upload dan promosi organisasimu juga, lho!</p>
+                    </div>
+                </div>
+                <div class="card text-dark bg-light mb-3 mx-3" style="max-width: 18rem;">
+                    <div class="card-body">
+                        <h4>Venue</h4>
+                        <p class="card-text">Temukan 2000+ Media Partner untuk dukung eventmu. Kamu bisa upload dan promosi organisasimu juga, lho!</p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- <div class=" bg-light m-2 ">
                 <img src="" alt="">
                 <h4>MedPart</h4>
                 <p>Temukan 2000+ Media Partner untuk dukung eventmu. Kamu bisa upload dan promosi organisasimu juga, lho!</p>
@@ -101,7 +137,9 @@
                 <img src="" alt="">
                 <h4>Venue</h4>
                 <p>Temukan 2000+ Media Partner untuk dukung eventmu. Kamu bisa upload dan promosi organisasimu juga, lho!</p>
-            </div>
+            </div> -->
+
+
         </div>
     </div>
 
