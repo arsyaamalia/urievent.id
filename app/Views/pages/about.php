b44e159b1f7007882a332502aa801bd94680b0b8
ochhochh <ochataniya@gmail.com>
2022-06-14 12:16:20 +0700

    ocha edit about

--- app/Views/pages/about.php
+++ app/Views/pages/about.php
@@ -2,15 +2,25 @@
 
 <?= $this->section('content'); ?>
 
-<div class="container">
-    <div class="row">
-        <div class="col">
-            <h1>ABOUT URIEVENT!</h1>
-            <p>This is about us pages</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus commodi, quae quos nihil amet dolor accusantium illo officia possimus sit, incidunt repellat fugiat esse, ea nisi sint. Maxime adipisci, cum deserunt consequatur fugiat laboriosam voluptatibus, ea ex sed ratione sint quam beatae error repudiandae doloremque neque accusantium fugit ut aliquid sequi molestiae eaque dicta? Provident alias quos consectetur optio nostrum non blanditiis quidem, officiis autem possimus maxime quam fugit beatae dolorum est repudiandae atque? Laborum itaque sint nemo nihil rerum? Dolorum ex quisquam porro accusamus odio consequuntur voluptatem laborum cupiditate, illum architecto obcaecati dolore facilis, totam ducimus repellat consectetur. Iure!</p>
+    <div class="container">
+        <div class="sub-one">
+            <h2>Lorem, ipsum dolor.</h2>
+            <h2>Lorem, ipsum dolor.</h2>
+            <h2>Lorem, ipsum dolor.</h2>
+            <div class="box">
 
+            </div>
+        </div>
+        <div class="sub-two">
+            <div class="box">
+
+            </div>
+        </div>
+        <div class="sub-three">
+            <div class="box">
+
+            </div>
         </div>
     </div>
-</div>
-
+</body>
 <?= $this->endsection(); ?>
