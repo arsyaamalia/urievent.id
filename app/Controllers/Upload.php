7aa08755ce9891c893ecf9ac64e4b7c0719df799
zzabinaan <zzabinaan@gmail.com>
2022-06-23 23:30:37 +0700

    link db dataKategori di pages/upload

--- app/Controllers/Upload.php
+++ app/Controllers/Upload.php
@@ -2,16 +2,29 @@
 
 namespace App\Controllers;
 
+use App\Models\kategori_layananModel;
+use App\Models\produk_layananModel;
+use App\Models\paket_layananModel;
+use App\Models\subkategori_layananModel;
+
 class Upload extends BaseController
 {
+    public function __construct()
+    {
+        $this->produk_layanan = new produk_layananModel();
+        $this->paket_layanan = new paket_layananModel();
+        $this->kategori_layanan = new kategori_layananModel();
+        $this->subkategori_layanan = new subkategori_layananModel();
+    }
 
 
     public function index()
     {
-
+        $dataKategori = $this->kategori_layanan->findAll();
+        $dataSubKategori = $this->subkategori_layanan;
         $dataPage = [
             'title' => "UriEvent | Upload Service",
-            'tes' => ['satu', 'dua', 'tiga']
+            'dataKategori' => $dataKategori
         ];
         return view('pages/upload', $dataPage);
     }
@@ -19,7 +32,6 @@ class Upload extends BaseController
     public function save()
     {
         $dataProduk = $this->request->getVar('company-name');
-        dd($dataProduk);
         $dataPaket = $this->paket_layanan->findAll();
         $dataKategori = $this->kategori_layanan->findAll();
         $dataSubKategori = $this->subkategori_layanan->findAll();
