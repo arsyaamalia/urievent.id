07d17f08282e8f67dba2ed47acfca330685baf65
zzabinaan <zzabinaan@gmail.com>
2022-06-26 05:35:14 +0700

    zeb coba

--- app/Views/pages/upload1.php
+++ app/Views/pages/upload1.php
@@ -341,10 +341,9 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 <script src="<?= base_url('/js/jquery-3.6.0.min.js') ?>"></script>
 <script src="<?= base_url('/js/script.js') ?>"></script>
-
 <script>
     $(document).ready(function() {
-        $('#category').change(function() {
+        $("#category").change(function() {
             var id_kategori = $(this).val();
             // console.log(id_kategori)
             $.ajax({
@@ -353,74 +352,17 @@
                 dataType: "JSON",
                 success: function(response) {
                     $.each(response, function(i, item) {
-                        $('#subcategory').append($('<option>', {
-                            value: item.id_subkategori,
-                            text: item.nama_subkategori
-                        }));
+                        $("#subcategory").append(
+                            $("<option>", {
+                                value: item.id_subkategori,
+                                text: item.nama_subkategori,
+                            })
+                        );
                     });
-                }
-            })
+                },
+            });
             // end ajax
         });
     });
-    //selecting all required elements
-    const dropArea = document.querySelector(".drag-area"),
-        dragText = dropArea.querySelector("header"),
-        button = dropArea.querySelector("button"),
-        input = dropArea.querySelector("input");
-    let file; //this is a global variable and we'll use it inside multiple functions
-
-    button.onclick = (e) => {
-        e.preventDefault()
-        input.click(); //if user click on the button then the input also clicked
-    }
-
-    input.addEventListener("change", function() {
-        //getting user select file and [0] this means if user select multiple files then we'll select only the first one
-        file = this.files[0];
-        dropArea.classList.add("active");
-        showFile(); //calling function
-    });
-
-
-    //If user Drag File Over DropArea
-    dropArea.addEventListener("dragover", (event) => {
-        event.preventDefault(); //preventing from default behaviour
-        dropArea.classList.add("active");
-        dragText.textContent = "Release an image";
-    });
-
-    //If user leave dragged File from DropArea
-    dropArea.addEventListener("dragleave", () => {
-        dropArea.classList.remove("active");
-        dragText.textContent = "Drag and drop an image";
-    });
-
-    //If user drop File on DropArea
-    dropArea.addEventListener("drop", (event) => {
-        event.preventDefault(); //preventing from default behaviour
-        //getting user select file and [0] this means if user select multiple files then we'll select only the first one
-        file = event.dataTransfer.files[0];
-        showFile(); //calling function
-    });
-
-    function showFile() {
-        let fileType = file.type; //getting selected file type
-        let validExtensions = ["image/jpeg", "image/jpg", "image/png"]; //adding some valid image extensions in array
-        if (validExtensions.includes(fileType)) { //if user selected file is an image file
-            let fileReader = new FileReader(); //creating new FileReader object
-            fileReader.onload = () => {
-                let fileURL = fileReader.result; //passing user file source in fileURL variable
-                // UNCOMMENT THIS BELOW LINE. I GOT AN ERROR WHILE UPLOADING THIS POST SO I COMMENTED IT
-                let imgTag = `<img src="${fileURL}" alt="image">`; //creating an img tag and passing user selected file source inside src attribute
-                dropArea.innerHTML = imgTag; //adding that created img tag inside dropArea container
-            }
-            fileReader.readAsDataURL(file);
-        } else {
-            alert("This is not an Image File!");
-            dropArea.classList.remove("active");
-            dragText.textContent = "Drag and drop an image";
-        }
-    }
 </script>
 <?= $this->endsection(); ?>
